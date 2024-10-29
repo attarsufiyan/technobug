2688c944e2a1e274a44867a94e7cbd109ecb056b
TestLab3108 <Sufiyan.TestLab@gmail.com>
2024-10-29 16:19:37 +0530

    In this the course browsing is been added.

--- src/test/java/utils/GenericUtils.java
+++ src/test/java/utils/GenericUtils.java
@@ -10,31 +10,28 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class GenericUtils {
-	
+
 	WebDriver driver;
-	
-	
-	 // Static method to wait for an element to be clickable using the default timeout
-    public static WebElement waitForElementToBeClickable(WebDriver driver, By locator) {
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
-        return wait.until(ExpectedConditions.elementToBeClickable(locator));
-    }
-    
-    // Static method to wait for an element to be visible
-    public static void visibilityOfElementLocated(WebDriver driver, By locator) {
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
-        wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
-    }
-    public static void scrollToBottom(WebDriver driver) {
-        JavascriptExecutor jsExecutor = (JavascriptExecutor) driver;
-        jsExecutor.executeScript("window.scrollTo(0, document.body.scrollHeight)");
-    }
 
-    public static void scrollBy(WebDriver driver, int x, int y) {
-        JavascriptExecutor jsExecutor = (JavascriptExecutor) driver;
-        jsExecutor.executeScript("window.scrollBy(arguments[0], arguments[1]);", x, y);
-    }
+	public static WebElement waitForElementToBeClickable(WebDriver driver, By locator) {
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+		return wait.until(ExpectedConditions.elementToBeClickable(locator));
+	}
 
+	// Static method to wait for an element to be visible
+	public static void visibilityOfElementLocated(WebDriver driver, By locator) {
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+		wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+	}
 
+	public static void scrollToBottom(WebDriver driver) {
+		JavascriptExecutor jsExecutor = (JavascriptExecutor) driver;
+		jsExecutor.executeScript("window.scrollTo(0, document.body.scrollHeight)");
+	}
+
+	public static void scrollBy(WebDriver driver, int x, int y) {
+		JavascriptExecutor jsExecutor = (JavascriptExecutor) driver;
+		jsExecutor.executeScript("window.scrollBy(arguments[0], arguments[1]);", x, y);
+	}
 
 }
